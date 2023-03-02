6054a047e31f5f4b3b6abec55aa535f0042ab510
mkogaha <124736415+mkogaha@users.noreply.github.com>
2023-03-03 05:41:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のUser</button>
+    </div>
+  );
+}
